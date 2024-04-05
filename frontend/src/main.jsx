b9abe854ff6ed07ad5e60834a0b200415cf0425b
Namrathakaki987 <147963673+Namrathakaki987@users.noreply.github.com>
2024-04-05 14:42:21 +0530

    Add files via upload
    adding frontend

---
+++ frontend/src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom';
+import App from './App.jsx'
+import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import {SnackbarProvider} from 'notistack'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+  <SnackbarProvider>
+  <App />
+
+  </SnackbarProvider>
+  </BrowserRouter>,
+)
